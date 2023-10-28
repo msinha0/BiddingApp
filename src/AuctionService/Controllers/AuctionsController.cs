e79eec234f3f1b4fe248c16649041d12861d6807
dev.msinha0@gmail.com <mayukhsinha11699@gmail.com>
2023-10-28 20:09:32 +0530

    Added publisher in Auction Service

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -7,6 +7,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,8 +20,10 @@ namespace AuctionService.Controllers
     {
         private readonly AuctionDbContext _context;
         private readonly IMapper _mapper;
-        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        private readonly IPublishEndpoint _publishEndpoint;
+        public AuctionsController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
         {
+            _publishEndpoint = publishEndpoint;
             _mapper = mapper;
             _context = context;
             
@@ -64,10 +68,15 @@ namespace AuctionService.Controllers
 
             var result = await _context.SaveChangesAsync() > 0;
 
+            var newAuction = _mapper.Map<AuctionDto>(auction);
+
+            //Publishes the message to all subscribed consumers for the Queue - RabbitMQ, MassTransit
+            await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+            
             if(!result) return BadRequest("Could not save changes to the DB");
 
             return CreatedAtAction(nameof(GetAuctionById), 
-                    new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+                    new {auction.Id}, newAuction);
         }
 
         [HttpPut("{id}")]
